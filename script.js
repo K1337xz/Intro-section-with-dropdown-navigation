54db1569c438ea827af8e7b7600463a4eb369453
Koporson <108631492+Koporson@users.noreply.github.com>
2022-07-27 23:26:11 +0200

    update

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const dropDown = document.querySelector(".dropdown");
 const secMenu = document.querySelector(".second");
 const secDown = document.querySelector(".seconddrop");
 const del = document.querySelector(".container");
+const closeMenu = document.querySelector(".close");
+const nav = document.querySelector("nav");
+const navMen = document.querySelector("nav > .menu");
+const hamb = document.querySelector(".hamburger");
+
 function openMenu() {
 	dropDown.classList.toggle("active");
 	if (secDown.classList.contains("active")) {
@@ -22,3 +27,20 @@ function delactiv() {
 menu.addEventListener("click", openMenu);
 secMenu.addEventListener("click", openSecond);
 del.addEventListener("click", delactiv);
+
+//open menu
+function openMobile() {
+	nav.style.setProperty("overflow", "visible");
+	navMen.style.setProperty("right", "0");
+	closeMenu.style.setProperty("display", "block");
+}
+
+//close menu function
+function closeMobile() {
+	nav.style.setProperty("overflow", "hidden");
+	navMen.style.setProperty("right", "-999px");
+	closeMenu.style.setProperty("display", "none");
+}
+
+closeMenu.addEventListener("click", closeMobile);
+hamb.addEventListener("click", openMobile);
